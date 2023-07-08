e8f405ad7a63b3c580e6626098e183a21f6def91
AlexUnknown <saint.kaizer@yandex.ru>
2023-07-08 16:09:55 +0300

    .\task_3\

---
+++ task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// в диапазоне от 10 до 1000 и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+// Или массив из 8 элементов вводится с консоли (каждый элемент вводит человек)
+
+Console.Write("Начальное значение для диапазона случайного числа массива: ");
+int minNum = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Конечное значение для диапазона случайного числа массива: ");
+int maxNum = Convert.ToInt32(Console.ReadLine());
+
+
+int size = 8;
+int [] array = new int[size]; 
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(minNum, maxNum + 1); 
+}
+Console.WriteLine($"[{string.Join("; ", array)}]"); 
