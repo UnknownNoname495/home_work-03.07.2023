af5ef7f60532b03ff815e398728d0159548a275d
AlexUnknown <saint.kaizer@yandex.ru>
2023-07-08 15:38:40 +0300

    .\task_1\

---
+++ task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите число A: ");
+string? stringNumberA = Console.ReadLine();
+
+Console.Write("Введите число B: ");
+string? stringNumberB = Console.ReadLine();
+
+int numA;
+int numB;
+
+bool isNumberA = int.TryParse(stringNumberA, out numA);
+bool isNumberB = int.TryParse(stringNumberB, out numB);
+
+int result = 1; 
+
+if (isNumberA && isNumberB)
+{ 
+    for (int i = 1; i <= numB; i++)
+    {
+        result = Convert.ToInt32(Math.Pow(numA, i));  
+    }
+    Console.WriteLine($"{stringNumberA} в степени {stringNumberB} = {result} ");
+}
+else
+{
+Console.WriteLine($"В значениях {stringNumberA} и {stringNumberB} находятся символы");
+}
